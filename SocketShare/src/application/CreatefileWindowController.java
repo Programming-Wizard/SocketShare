610d4493b7000c364a4b2bde491432efb22c8c17
Programming-Wizard <varun.02.11.04@gmail.com>
2024-01-22 03:39:40 +0530

    now the user can write and send a file without having to have a file present on machine

---
+++ SocketShare/src/application/CreatefileWindowController.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class CreatefileWindowController implements Initializable{
+	
+	@FXML
+	private TextField fileName;
+	@FXML
+	private Button SendButton;
+	@FXML
+	private TextArea FileContent;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+	}
+	public TextArea getFileContent() {
+		return FileContent;
+	}
+	public TextField getFileName() {
+		return fileName;
+	}
+	public Button getSendButton() {
+		return SendButton;
+	}
+
+}
--- SocketShare/src/application/MainWindowController.java
+++ SocketShare/src/application/MainWindowController.java
@@ -1,8 +1,17 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.net.URL;
+import java.net.UnknownHostException;
+import java.nio.file.Files;
 import java.util.ResourceBundle;
 
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -10,11 +19,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyCombination;
-import javafx.scene.text.Text;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-
 
 public class MainWindowController implements Initializable{
 	
@@ -23,6 +28,8 @@ public class MainWindowController implements Initializable{
 	@FXML
 	private Button CreateAndSendButton;
 	
+	private String fileName = "";
+	private String fileContent = "";
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -44,19 +51,23 @@ public class MainWindowController implements Initializable{
 			CreateFileStage.setMaximized(true);
 			CreateFileStage.setResizable(true); 
 			CreateFileStage.show();
+			CreatefileWindowController controller = CreateFileLoader.getController();
 			CreateFileStage.setOnHidden(event->{
-//	        	controller.getChooseFileButton().setDisable(false);
+				getChooseFileButton().setDisable(false);
 	        });
 			CreateFileStage.getScene().getAccelerators().put(KeyCombination.keyCombination("CTRL+W"), new Runnable() {
 				@Override
 				public void run() {
-//					controller.getChooseFileButton().setDisable(false);
+					getChooseFileButton().setDisable(false);
 					CreateFileStage.close();
 				}
 			});
+			controller.getSendButton().setOnAction(event->{
+				sendFileToServer(controller);
+			});
 			
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();	
 		}
 	}
 	
@@ -65,5 +76,58 @@ public class MainWindowController implements Initializable{
 	}	
 	public Button getCreateAndSendButton() {
 		return CreateAndSendButton;
+	}
+	private void sendFileToServer(CreatefileWindowController controller) {
+		Task<Void> SendTask = new Task<>() {
+
+			@Override
+			protected Void call() throws Exception {
+				IPS ip = new IPS();
+				String[] serverIPs = {ip.getZorinIP(),ip.getXubuntuIP()};
+		        int port = 6961;
+//		        for (String serverIP : serverIPs) {
+		            InetSocketAddress serverAddress = new InetSocketAddress(serverIPs[2], port);
+		            try {
+		                String fileNameRegex = "^[^\\s]+(?:[_-][^\\s]+)*(\\.[^.]+)$";
+		                String contentToSend = controller.getFileContent().getText();
+		                if(!controller.getFileName().getText().trim().matches(fileNameRegex) || controller.getFileName().getText().trim().isEmpty() || contentToSend.trim().isEmpty()) {
+		                	return null;
+		                }
+		                
+		                System.out.println("Sending ...");
+		                byte[] fileBytes = contentToSend.getBytes();
+		                Socket socket = new Socket();
+
+		                try {
+		                    socket.connect(serverAddress);
+		                } catch (IOException e) {
+		                    System.err.println("Failed to connect to " + serverAddress);
+		                    e.printStackTrace();
+//		                    continue;
+		                }
+
+//		                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+		                output.println(controller.getFileName().getText());
+		                output.flush();
+
+		                OutputStream outputStream = socket.getOutputStream();
+		                outputStream.write(fileBytes);
+		                outputStream.flush();
+
+		                System.out.println("Connected to: " + serverAddress);
+
+		                socket.close();
+//		                break;
+		            } catch (UnknownHostException e) {
+		                e.printStackTrace();
+		            } catch (IOException e) {
+		                e.printStackTrace();
+		            }
+//		        }
+				return null;
+			}};
+			Thread sendTaskThread = new Thread(SendTask);
+			sendTaskThread.start();
 	}
 }
